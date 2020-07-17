4bcf49c54e451539197c18fe237951afe1ab6e12
henriquecatunda <henriquecatunda2@gmail.com>
2020-07-16 22:06:31 -0300

    adicionar style

--- script.js
+++ script.js
@@ -52,6 +52,17 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && diretion == "down") snake[0].y = 0;
     if(snake[0].y < 0  && diretion == "up") snake[0].y = 16 * box;
 
+
+    for(i=1 ; i < snake.length;i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+
+            clearInterval(jogo);
+            alert("Game Over");
+        }
+    }
+
+
+
     criarBG();
     criarCobrinha();
     drawFood();
